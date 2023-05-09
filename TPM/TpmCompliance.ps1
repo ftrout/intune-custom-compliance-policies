f9a832a50f02895b8919ac49551a2165ddb40447
ftrout@live.com <ftrout@live.com>
2023-05-09 15:09:51 -0400

    Updated

--- TPM/TpmCompliance.ps1
+++ TPM/TpmCompliance.ps1
@@ -1,6 +1,5 @@
 $tpm = Get-Tpm
-$tpmSpecVersion = $((Get-WmiObject -Namespace "root\cimv2\security\microsofttpm" -Class Win32_Tpm).SpecVersion -split ",")[0]
-$tpmVersion = $tpmSpecVersion.ToString()
+[uint64]$tpmVersion = $((Get-WmiObject -Namespace "root\cimv2\security\microsofttpm" -Class Win32_Tpm).SpecVersion -split ",")[0]
 
 $hash = @{ TpmPresent = $tpm.TpmPresent; TpmVersion = $tpmVersion; TpmReady = $tpm.TpmReady; TpmEnabled = $tpm.TpmEnabled; TpmActivated = $tpm.TpmActivated; RestartPending = $tpm.RestartPending }
 return $hash | ConvertTo-Json -Compress
