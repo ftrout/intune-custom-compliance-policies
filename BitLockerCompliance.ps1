6cf9720fffbfad52d1155df6594088e37b5c8ff3
ftrout@live.com <ftrout@live.com>
2023-05-29 19:04:20 -0400

    Updated BitlockerCompliance script and settings

--- BitLockerCompliance.ps1
+++ BitLockerCompliance.ps1
@@ -1,4 +1,10 @@
 $bitlockerStatus = Get-BitLockerVolume -MountPoint $env:SystemDrive
 
-$hash = @{ EncryptionMethod = $bitlockerStatus.EncryptionMethod.ToString(); VolumeStatus = $bitlockerStatus.VolumeStatus.ToString(); ProtectionStatus = $bitlockerStatus.ProtectionStatus.ToString(); EncryptionPercentage = $bitlockerStatus.EncryptionPercentage.ToString() }
+$hash = @{ 
+    EncryptionMethod     = $bitlockerStatus.EncryptionMethod.ToString()
+    VolumeStatus         = $bitlockerStatus.VolumeStatus.ToString()
+    ProtectionStatus     = $bitlockerStatus.ProtectionStatus.ToString()
+    EncryptionPercentage = $bitlockerStatus.EncryptionPercentage.ToString() 
+}
+
 return $hash | ConvertTo-Json -Compress
